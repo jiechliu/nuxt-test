5c64b610adb07507eb184a41d014c60a61b2c20d
v_jiechliu <v_jiechliu@tencent.com>
2025-11-06 10:37:19 +0800

    feat: font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { fonts } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
@@ -15,7 +15,7 @@ export default function Page() {
           <div
             className="relative w-0 h-0 border-l-15 border-r-15 border-b-26 border-l-transparent border-r-transparent border-b-black"
           />
-          <p className={`${fonts.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lusitana } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const fonts = Lusitana({
+export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700' ]
 })
